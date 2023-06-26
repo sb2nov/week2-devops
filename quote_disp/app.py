a3fc3e41cd1dea63e6064fa79584c56772f3b0cb
wooja-sou <112561121+wooja-sou@users.noreply.github.com>
2023-06-26 19:26:54 +0000

    created images, containers and network

--- quote_disp/app.py
+++ quote_disp/app.py
@@ -18,8 +18,16 @@ def home():
 
 @app.route("/get_quote")
 def quote():
-    quote = requests.get("http://gen:5000/quote").text
-    print("quote - ", quote)
+    hosts = [
+        "http://week2-devops-jwooden_web1_1:5000/quote",
+        "http://week2-devops-jwooden_web1_2:5000/quote"
+    ]
+    quote = "Quote service is unavailable"
+    for host in hosts:
+        r = requests.get(host)
+        if r.status_code == 200:
+            quote = r.text
+            break
 
     return render_template("quote.html", quote=quote)
 
